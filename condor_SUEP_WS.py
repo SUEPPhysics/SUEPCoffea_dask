7aa28e698d7c66fcafd81db1760b0dbffb8bc320
Chad Freer <freerc@submit01.mit.edu>
2021-12-16 14:48:31 -0500

    changing from xsec to gensumweight (plotter update to use gensumweight) 	modified:   condor_SUEP_WS.py 	modified:   workflows/SUEP_coffea.py 	deleted:    workflows/SumWeights.py

--- condor_SUEP_WS.py
+++ condor_SUEP_WS.py
@@ -7,7 +7,6 @@ from coffea.processor import run_uproot_job, futures_executor
 
 #SUEP Repo Specific
 from workflows.SUEP_coffea import *
-from workflows.SumWeights import *
 
 #Begin argparse
 parser = argparse.ArgumentParser("")
@@ -20,43 +19,10 @@ parser.add_argument('--dataset', type=str, default="X", help="")
 parser.add_argument('--nevt', type=str, default=-1, help="")
 options = parser.parse_args()
 
-#Set up cross section for MC normalizations
-xsection = 1.0
-if options.isMC:
-   with open(os.path.dirname(__file__) +'data/xsections_{}.json'.format(options.era)) as file:
-       MC_xsecs = json.load(file)
-   try:
-       xsection *= MC_xsecs[options.dataset]["xsec"]
-       xsection *= MC_xsecs[options.dataset]["kr"]
-       xsection *= MC_xsecs[options.dataset]["br"]
-   except:
-       print("WARNING: I did not find the xsection for that MC sample. Check the dataset name and the relevant yaml file")
-
-#Take care of the normalization factor. Returns a float that is the xsec/gensumweight. Branching fractions and kfactors are included(see above).
-
-#modules_gensum = []
-#if options.isMC:
-#    modules_gensum.append(XsecSumWeight(isMC=options.isMC, xsec = xsection,  era=int(options.era), do_syst=1, syst_var='', sample=options.dataset))
-#
-#    for instance in modules_gensum:
-#        output = run_uproot_job(
-#            {instance.sample: [options.infile]},
-#            treename='Runs',
-#            processor_instance=instance,
-#            executor=futures_executor,
-#            executor_args={'workers': 10,
-#                           'schema': processor.NanoAODSchema,
-#                           'xrootdtimeout': 10,
-#            },
-##            chunksize=500000000
-#        )
-#        xsection2 = output
-
 out_dir = os.getcwd()
 modules_era = []
 #Run the SUEP code. Note the xsection as input. For Data the xsection = 1.0 from above
-modules_era.append(SUEP_cluster(isMC=options.isMC, era=int(options.era), do_syst=1, xsec = xsection,  syst_var='', sample=options.dataset, weight_syst='' , flag=False, output_location=out_dir))
-    
+modules_era.append(SUEP_cluster(isMC=options.isMC, era=int(options.era), do_syst=1,  syst_var='', sample=options.dataset, weight_syst='' , flag=False, output_location=out_dir))
 
 for instance in modules_era:
     output = run_uproot_job(
@@ -68,5 +34,5 @@ for instance in modules_era:
                        'schema': processor.NanoAODSchema,
                        'xrootdtimeout': 10,
         },
-        chunksize=50000
+        chunksize=500000
     )
