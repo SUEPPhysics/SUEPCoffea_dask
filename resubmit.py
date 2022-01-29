552cdf31c8658544394345dda8720a6e23dcacc3
lavezzo <llavez99@gmail.com>
2022-01-29 17:04:14 -0500

    Commented out mult, fixed empty dataframe case, and added number of ak4jet. Changes to resubmission script

--- resubmit.py
+++ resubmit.py
@@ -6,16 +6,14 @@ import time
 logging.basicConfig(level=logging.DEBUG)
 
 parser = argparse.ArgumentParser(description='Famous Submitter')
-parser.add_argument("-r"   , "--resubmits" , type=int, default=5     , help="Number of resubmissions.", required=True)
-parser.add_argument("-n"   , "--hours"     , type=int, default=2  , help="Number of hours per resubmission.", required=True)
+parser.add_argument("-r"   , "--resubmits" , type=int, default=10     , help="Number of resubmissions.", required=False)
+parser.add_argument("-n"   , "--hours"     , type=float, default=1.0  , help="Number of hours per resubmission.", required=False)
 parser.add_argument("-t"   , "--tag"       , type=str, help="Dataset tag.")
-parser.add_argument("-i"   , "--input"     , type=str, help="Input filelist.")
 
 options = parser.parse_args()
 nResubmits = options.resubmits
 nHours = options.hours
 tag = options.tag
-filelist = options.input
 
 # Making sure that the proxy is good
 proxy_base = 'x509up_u{}'.format(os.getuid())
@@ -51,9 +49,27 @@ for i in range(nResubmits):
     logging.info("Resubmission "+str(i))
     logging.info("Removing all jobs...")
     os.system('condor_rm {}'.format(os.environ['USER']))
-    logging.info("Executing monitor.py...")
-    os.system("python3 monitor.py --tag={} --input={} -r=1".format(tag, filelist))
+    logging.info("Checking for corrupted files and removing them...")
+    
+    # delete files that are corrupted
+    dataDir = "/mnt/T3_US_MIT/hadoop/scratch/{}/SUEP/{}/".format(username, tag)
+    subdirs = os.listdir(dataDir)
+    for subdir in subdirs:
+        for file in os.listdir(dataDir + subdir):
+            size = os.path.getsize(dataDir + subdir + "/" + file)
+            if size == 0: 
+                #print(dataDir + subdir + "/" + file)
+                subprocess.run(['rm',dataDir + subdir + "/" + file])
+        
+    logging.info("Executing monitor.py for data...")
+    os.system("python3 monitor.py --tag={} --input={} -r=1".format(tag, 'filelist/list_test.txt'))
+    logging.info("Executing monitor.py for MC...")
+    os.system("python3 monitor.py --tag={} --input={} -r=1".format(tag, 'filelist/list_2018_MC_A01.txt'))
+    
     # don't wait if it's the last submission
-    if i == nResubmits - 1: break
-    logging.info("Sleeping for",nHours,"hours...")
+    if i == nResubmits - 1: 
+        logging.info("All done")
+        break
+        
+    logging.info("Sleeping for "+str(nHours)+" hours...")
     time.sleep(sleepTime)
