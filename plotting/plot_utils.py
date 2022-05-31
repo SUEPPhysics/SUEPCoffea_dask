0e7a280c115224adf326d1e04738e9f49446f2e6
Luca Lavezzo <llavez99@gmail.com>
2022-05-31 11:46:20 -0400

    added rebin functions

--- plotting/plot_utils.py
+++ plotting/plot_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import defaultdict
 import pickle
+import boost_histogram as bh
 
 default_colors = {
     'QCD': 'midnightblue',
@@ -251,6 +252,55 @@ def combine_bins(x, y, e, n=4):
         e.append(np.sqrt(np.sum(old_e[j:j+n]**2)))
         
     return np.array(x), np.array(y), np.array(e)
+
+def rebin_piecewise_constant(x1, y1, x2):
+    x1 = np.asarray(x1)
+    y1 = np.asarray(y1)
+    x2 = np.asarray(x2)
+
+    # the fractional bin locations of the new bins in the old bins
+    i_place = np.interp(x2, x1, np.arange(len(x1)))
+    cum_sum = np.r_[[0], np.cumsum(y1)]
+
+    # calculate bins where lower and upper bin edges span
+    # greater than or equal to one original bin.
+    # This is the contribution from the 'intact' bins (not including the
+    # fractional start and end parts.
+    whole_bins = np.floor(i_place[1:]) - np.ceil(i_place[:-1]) >= 1.
+    start = cum_sum[np.ceil(i_place[:-1]).astype(int)]
+    finish = cum_sum[np.floor(i_place[1:]).astype(int)]
+    y2 = np.where(whole_bins, finish - start, 0.)
+    bin_loc = np.clip(np.floor(i_place).astype(int), 0, len(y1) - 1)
+
+    # fractional contribution for bins where the new bin edges are in the same
+    # original bin.
+    same_cell = np.floor(i_place[1:]) == np.floor(i_place[:-1])
+    frac = i_place[1:] - i_place[:-1]
+    contrib = (frac * y1[bin_loc[:-1]])
+    y2 += np.where(same_cell, contrib, 0.)
+
+    # fractional contribution for bins where the left and right bin edges are in
+    # different original bins.
+    different_cell = np.floor(i_place[1:]) > np.floor(i_place[:-1])
+    frac_left = np.ceil(i_place[:-1]) - i_place[:-1]
+    contrib = (frac_left * y1[bin_loc[:-1]])
+    frac_right = i_place[1:] - np.floor(i_place[1:])
+    contrib += (frac_right * y1[bin_loc[1:]])
+    y2 += np.where(different_cell, contrib, 0.)
+
+    return y2
+
+def rebin(h, bins):
+
+    current_bins, current_edges = h.to_numpy()
+    new_freq = rebin_piecewise_constant(current_edges, h.values(), bins)
+    new_variances = rebin_piecewise_constant(current_bins, h.variances(), bins)
+    
+    h = bh.Histogram(bh.axis.Variable(bins),storage=bh.storage.Weight())
+
+    h[:] = np.stack([new_freq, new_variances], axis=-1)
+    
+    return h
 
 def nested_dict(n, type):
     if n == 1:
