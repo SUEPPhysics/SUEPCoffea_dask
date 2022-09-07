aa0be7a5946d8c99260322ea667304bd381599cb
Weijie <2644185654@qq.com>
2022-09-07 19:03:37 +0200

    add PSWeight

--- workflows/SUEP_coffea.py
+++ workflows/SUEP_coffea.py
@@ -471,7 +471,15 @@ class SUEP_cluster(processor.ProcessorABC):
         if self.scouting == 1:
             self.out_vars["PV_npvs"] = ak.num(events.Vertex.x)
         else:
-            if self.isMC: self.out_vars["Pileup_nTrueInt"] = events.Pileup.nTrueInt
+            if self.isMC:
+                self.out_vars["Pileup_nTrueInt"] = events.Pileup.nTrueInt
+                if len(events.PSWeight[0])==4:
+                    self.out_vars["PSWeight_ISR_up"] = events.PSWeight[:,0]
+                    self.out_vars["PSWeight_ISR_down"] = events.PSWeight[:,2]
+                    self.out_vars["PSWeight_FSR_up"] = events.PSWeight[:,1]
+                    self.out_vars["PSWeight_FSR_down"] = events.PSWeight[:,3]
+                else:
+                    self.out_vars["PSWeight"] = events.PSWeight[:,0]
             self.out_vars["PV_npvs"] = events.PV.npvs
             self.out_vars["PV_npvsGood"] = events.PV.npvsGood
     
