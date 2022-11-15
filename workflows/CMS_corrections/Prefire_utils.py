c3c14c9cd49076026ee1df7da261451cb3211582
chad <chadwfreer@gmail.com>
2022-11-15 15:21:42 -0500

    adding in the corrected prefire fix 	modified:   plotting/check_hdf5.ipynb 	modified:   workflows/CMS_corrections/Prefire_utils.py 	modified:   workflows/SUEP_coffea.py

--- workflows/CMS_corrections/Prefire_utils.py
+++ workflows/CMS_corrections/Prefire_utils.py
@@ -1,9 +1,9 @@
 def GetPrefireWeights(self, events):
 
     if self.era == 2016 or self.era == 2017:
-        self.out_vars["prefire_nom"] = events.L1PreFiringWeight_Nom
-        self.out_vars["prefire_up"] = events.L1PreFiringWeight_Up
-        self.out_vars["prefire_down"] = events.L1PreFiringWeight_Dn
+        self.out_vars["prefire_nom"] = events.L1PreFiringWeight.Nom
+        self.out_vars["prefire_up"] = events.L1PreFiringWeight.Up
+        self.out_vars["prefire_down"] = events.L1PreFiringWeight.Dn
     else:
         self.out_vars["prefire_nom"] = 1.0
         self.out_vars["prefire_up"] = 1.0
