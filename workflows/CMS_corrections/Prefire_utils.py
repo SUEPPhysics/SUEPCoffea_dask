bd3d1b8862811a7bef274249163f6004118b1d58
Luca Lavezzo <llavez99@gmail.com>
2023-01-25 10:10:01 -0500

    finished integrating pandas accumulator

--- workflows/CMS_corrections/Prefire_utils.py
+++ workflows/CMS_corrections/Prefire_utils.py
@@ -1,11 +1,11 @@
 def GetPrefireWeights(self, events):
 
     if self.era == 2016 or self.era == 2017:
-        self.out_vars["prefire_nom"] = events.L1PreFiringWeight.Nom
-        self.out_vars["prefire_up"] = events.L1PreFiringWeight.Up
-        self.out_vars["prefire_down"] = events.L1PreFiringWeight.Dn
+        self._accumulator['vars']["prefire_nom"] = events.L1PreFiringWeight.Nom
+        self._accumulator['vars']["prefire_up"] = events.L1PreFiringWeight.Up
+        self._accumulator['vars']["prefire_down"] = events.L1PreFiringWeight.Dn
     else:
-        self.out_vars["prefire_nom"] = 1.0
-        self.out_vars["prefire_up"] = 1.0
-        self.out_vars["prefire_down"] = 1.0
+        self._accumulator['vars']["prefire_nom"] = 1.0
+        self._accumulator['vars']["prefire_up"] = 1.0
+        self._accumulator['vars']["prefire_down"] = 1.0
     return
