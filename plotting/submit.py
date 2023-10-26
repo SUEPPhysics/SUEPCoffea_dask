ff826063ec74331125a583743ce9e698ca57fb47
Luca Lavezzo <llavez99@gmail.com>
2023-10-25 22:09:48 -0400

    fix env in submit.py

--- plotting/submit.py
+++ plotting/submit.py
@@ -37,9 +37,10 @@ slurm_script_template = """#!/bin/bash
 
 source ~/.bashrc
 export X509_USER_PROXY=/home/submit/{user}/{proxy}
-#source activate env
-conda activate SUEP # Change to your own environment setup
 cd {work_dir}
+cd ..
+source setup.sh
+cd plotting/
 {cmd}
 """
 
@@ -187,12 +188,12 @@ for i, sample in enumerate(samples):
 
     # Code to execute
     if options.code == "merge":
-        cmd = "python3 merge_plots.py --tag={tag} --dataset={sample} --isMC={isMC}".format(
+        cmd = "suepRun merge_plots.py --tag={tag} --dataset={sample} --isMC={isMC}".format(
             tag=options.tag, sample=sample, isMC=options.isMC
         )
 
     elif options.code == "plot":
-        cmd = "python3 make_plots.py --dataset={sample} --tag={tag} --output={output_tag} --xrootd={xrootd} --weights={weights} --isMC={isMC} --era={era} --scouting={scouting} --merged={merged} --doInf={doInf} --doABCD={doABCD} --doSyst={doSyst} --blind={blind} --predictSR={predictSR} --save={save} --channel={channel}".format(
+        cmd = "suepRun make_plots.py --dataset={sample} --tag={tag} --output={output_tag} --xrootd={xrootd} --weights={weights} --isMC={isMC} --era={era} --scouting={scouting} --merged={merged} --doInf={doInf} --doABCD={doABCD} --doSyst={doSyst} --blind={blind} --predictSR={predictSR} --save={save} --channel={channel}".format(
             sample=sample,
             tag=options.tag,
             output_tag=options.output,
